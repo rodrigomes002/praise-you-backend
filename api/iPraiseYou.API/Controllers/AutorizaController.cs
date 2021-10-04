61705387af898f0aa0eede07f8363ebe40795e27
Rodrigo Gomes <rodrigomes002@gmail.com>
2021-10-04 20:29:56 -0300

    criando autorização

---
+++ api/iPraiseYou.API/Controllers/AutorizaController.cs
@@ -0,0 +1,83 @@
+﻿using iPraiseYou.API.DTO;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iPraiseYou.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutorizaController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AutorizaController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "AutorizaController :: Acessado em : " + DateTime.Now.ToLongDateString();
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> RegisterUser([FromBody] UsuarioDTO dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.Values.SelectMany(e => e.Errors));
+            }
+
+            var user = new IdentityUser
+            {
+                UserName = dto.Email,
+                Email = dto.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, dto.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            await _signInManager.SignInAsync(user, false);
+            return Ok(GeraToken(dto));
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] UsuarioDTO dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.Values.SelectMany(e => e.Errors));
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(dto.Email,
+                dto.Password, isPersistent: false, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return Ok(GeraToken(dto));
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Login inválido....");
+                return BadRequest(ModelState);
+            }
+        }
+
+
+        private UsuarioDTO GeraToken(UsuarioDTO dto)
+        {
+            return dto;
+        }
+    }
+}
---
+++ api/iPraiseYou.API/DTO/UsuarioDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace iPraiseYou.API.DTO
+{
+    public class UsuarioDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmedPassword { get; set; }
+    }
+}
