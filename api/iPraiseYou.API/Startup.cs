215c5554dd6b7ba53d91e802e003b35ba610e014
Rodrigo Gomes Calixto da Silva <rodrigo.calixto@radixeng.com.br>
2021-10-13 20:12:31 -0300

    ajustando para db in memory

--- api/iPraiseYou.API/Startup.cs
+++ api/iPraiseYou.API/Startup.cs
@@ -25,7 +25,8 @@ namespace iPraiseYou.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            //services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase("iPraiseDB"));
 
             services.AddIdentity<IdentityUser, IdentityRole>()
                 .AddEntityFrameworkStores<DataContext>()
